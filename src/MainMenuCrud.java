60aead38d29bb71eb1bb4149a675aee44aad8b73
azamatkhu <azamat.khu@educa.jcyl.es>
2026-04-21 13:19:32 +0200

    Bulk upload

--- src/MainMenuCrud.java
+++ src/MainMenuCrud.java
@@ -1,15 +1,133 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.*;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class MainMenuCrud {
+    public static void main(String[] args) {
+        int opcion = -1;
+        Scanner sc = new Scanner(System.in);
+        String sql = "";
+
+        while(opcion != 0) {
+            System.out.println("----- MENU SQL------");
+            System.out.println("1. Insertar empleado");
+            System.out.println("2. Mostrar empleado");
+            System.out.println("3. Actualizar empleado");
+            System.out.println("4. Eliminar empleado");
+            System.out.println(" ");
+            System.out.println("0. Salir");
+            System.out.println("Elige la opcion: ");
+
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            try {
+                Connection connection = DriverManager.getConnection(
+                        "jdbc:oracle:thin:@localhost:1521:xe",
+                        "RIBERA",
+                        "ribera"
+                );
+
+                System.out.println("Conectado!");
+
+                Statement statement = connection.createStatement();
+
+                PreparedStatement ps;
+
+                switch(opcion) {
+                    case 1:
+                        System.out.println("Introduce el identificador del empleado: ");
+                        int idEmpleado = sc.nextInt();
+                        sc.nextLine();
+
+                        System.out.println("Introduce el nombre del empleado: ");
+                        String nombreEmpleado = sc.nextLine();
+
+                        System.out.println("Introduce el salario del empleado: ");
+                        double salarioEmpleado = sc.nextDouble();
+
+                        sql = "INSERT INTO EMPLEADO (id, nombre, salario) VALUES (?, ?, ?)";
+                        ps = connection.prepareStatement(sql);
+
+                        ps.setInt(1, idEmpleado);
+                        ps.setString(2, nombreEmpleado);
+                        ps.setDouble(3, salarioEmpleado);
+
+                        ps.executeUpdate();
+
+                        System.out.println("Se ha insertado el empleado " + idEmpleado + "!");
+                        break;
+                    case 2:
+                        sql = "SELECT * FROM EMPLEADO WHERE ID = ?";
+                        Statement state = connection.createStatement();
+
+                        ResultSet resultSet = state.executeQuery(sql);
+
+                        while (resultSet.next()) {
+                            System.out.println(resultSet.getInt("id") + " " + resultSet.getString("nombre") + " " + resultSet.getDouble("salario"));
+                        }
+
+                        break;
+                    case 3:
+                        System.out.println("Introduce el identificador del empleado: ");
+                        int idActualizar = sc.nextInt();
+                        sc.nextLine();
+
+                        System.out.println("Que campo quieres actualizar? ");
+                        String campo = sc.nextLine();
+                        switch(campo) {
+                            case "nombre":
+                                System.out.println("Introduce el nombre del empleado: ");
+                                String nombreActualizar = sc.nextLine();
+
+                                sql = "UPDATE EMPLEADO SET NOMBRE = ? WHERE ID = ?";
+                                ps = connection.prepareStatement(sql);
+                                ps.setString(1, nombreActualizar);
+                                ps.setInt(2, idActualizar);
+
+                                ps.executeUpdate();
+                                break;
+                            case "salario":
+                                System.out.println("Introduce el salario del empleado: ");
+                                double salarioActualizar = sc.nextDouble();
+
+                                sql = "UPDATE EMPLEADO SET SALARIO = ? WHERE ID = ?";
+                                ps = connection.prepareStatement(sql);
+                                ps.setDouble(1, salarioActualizar);
+                                ps.setInt(2, idActualizar);
+
+                                ps.executeUpdate();
+                                break;
+                            default:
+                                System.out.println("No existe este campo!");
+
+                                break;
+                        }
+
+                        System.out.println("Se ha modificado el empleado " + idActualizar + "!");
+
+                        break;
+                    case 4:
+                        System.out.println("Introduce el identificador del empleado: ");
+                        int idEliminar = sc.nextInt();
+                        sc.nextLine();
+
+                        sql = "DELETE FROM EMPLEADO WHERE ID = ?";
+                        ps = connection.prepareStatement(sql);
+                        ps.setInt(1, idEliminar);
+
+                        ps.executeUpdate();
+                        System.out.println("Se ha eliminado  el empleado " + idEliminar + "!");
+                        break;
+                    case 0:
+                        System.out.println("Saliendo....");
+                        break;
+                    default:
+                        System.out.println("La opcion no es valida!");
+                        break;
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
